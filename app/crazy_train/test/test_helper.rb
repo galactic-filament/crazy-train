ae61e3399e45e073d1ec167dfa9c656ab3c5391b
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-03-06 19:44:30 -0700

    Add pushing to coveralls.io.

--- app/crazy_train/test/test_helper.rb
+++ app/crazy_train/test/test_helper.rb
@@ -2,6 +2,19 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+require "simplecov"
+require "simplecov-lcov"
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::LcovFormatter,
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start do
+  add_filter(/^\/spec\//) # For RSpec
+  add_filter(/^\/test\//) # For Minitest
+end
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
