eae88434ef9d8bbc23ffb3841ca822fbcef3bebf
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-03-06 20:09:30 -0700

    Fix gem version for simplecov, clean up simplecov config.

--- app/crazy_train/test/test_helper.rb
+++ app/crazy_train/test/test_helper.rb
@@ -5,15 +5,19 @@ require "rails/test_help"
 require "simplecov"
 require "simplecov-lcov"
 
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::LcovFormatter,
-  SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start do
-  add_filter(/^\/spec\//) # For RSpec
-  add_filter(/^\/test\//) # For Minitest
+if ENV["CI"] == "true"
+  SimpleCov::Formatter::LcovFormatter.config do |config|
+    config.report_with_single_file = true
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::LcovFormatter
+  ])
+else
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 end
+
+SimpleCov.start("rails")
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
