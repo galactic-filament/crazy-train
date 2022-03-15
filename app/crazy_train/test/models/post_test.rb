0019b7c88030baecebfd2bc44bd4a829a591b4de
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-03-14 19:25:15 -0700

    Fix typo.

--- app/crazy_train/test/models/post_test.rb
+++ app/crazy_train/test/models/post_test.rb
@@ -3,9 +3,7 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
   test "post create happy-path" do
     post = Post.new(body: "Hello, world!")
-    # post.save!
-
-    puts "Post id: #{post.id}"
+    post.save!
 
     assert_not_equal post.id, nil
   end
