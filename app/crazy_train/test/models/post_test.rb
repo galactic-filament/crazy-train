019934aee5d0e2ceb06a044bc39667011309892d
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-03-18 16:41:21 -0700

    Add validation rule for post body.

--- app/crazy_train/test/models/post_test.rb
+++ app/crazy_train/test/models/post_test.rb
@@ -1,10 +1,15 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  test "post create happy-path" do
+  test "post save happy-path" do
     post = Post.new(body: "Hello, world!")
     post.save!
 
     assert_not_equal post.id, nil
+  end
+
+  test "post flag invalid with missing body" do
+    post = Post.new(body: "")
+    assert_equal post.valid?, false
   end
 end
