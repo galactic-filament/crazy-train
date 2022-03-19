b3d9f24b55fbd92605cd8ce199f2b0d596993ae7
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-03-19 13:56:35 -0700

    Fill in get-post functionality.

--- app/crazy_train/app/controllers/posts_controller.rb
+++ app/crazy_train/app/controllers/posts_controller.rb
@@ -4,4 +4,13 @@ class PostsController < ApplicationController
 
     render json: {posts: posts}
   end
+
+  def show
+    post = Post.find_by params[:id]
+    if post.nil?
+      raise ActiveRecord::RecordNotFound
+    end
+
+    render json: {post: post}
+  end
 end
--- app/crazy_train/test/controllers/posts_controller_test.rb
+++ app/crazy_train/test/controllers/posts_controller_test.rb
@@ -4,6 +4,41 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should return empty list of posts" do
     get "/posts"
     assert_equal @response.status, 200
-    assert_equal @response.body, {posts: []}.to_json
+
+    parsed_response = JSON.parse @response.body
+    assert_equal parsed_response["posts"].size, 0
+  end
+
+  test "should return correct list of posts" do
+    post = Post.new(body: "Hello, world")
+    post.save!
+
+    get "/posts"
+    assert_equal @response.status, 200
+
+    parsed_response = JSON.parse @response.body
+    assert_equal parsed_response["posts"].size, 1
+  end
+
+  test "should return not found for non-exist post" do
+    err = nil
+    begin
+      get "/posts/0"
+    rescue ActiveRecord::RecordNotFound => e
+      err = e
+    end
+
+    assert_not_equal err, nil
+  end
+
+  test "should existing post" do
+    post = Post.new(body: "Hello, world!")
+    post.save!
+
+    get "/posts/#{post.id}"
+    assert_equal @response.status, 200
+
+    parsed_response = JSON.parse @response.body
+    assert_equal parsed_response["post"]["id"], post.id
   end
 end
